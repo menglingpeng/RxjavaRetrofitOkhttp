ca9dec7bc26012038945a94451c87c65cd6c3f79
menglingpeng <menglingpeng@live.com>
2018-04-03 16:41:33 +0800

    feat: add the function of changeBaseUrl for RetrofitClient

--- app/src/main/java/RetrofitClient.java
+++ app/src/main/java/RetrofitClient.java
@@ -2,10 +2,13 @@ import android.content.Context;
 import android.text.TextUtils;
 
 import com.menglingpeng.httputil.api.ApiService;
+import com.menglingpeng.httputil.base.BaseInterceptor;
 
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Cache;
+import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -22,7 +25,9 @@ public class RetrofitClient {
     public static String baseUrl = ApiService.Base_URL;
     private ApiService apiService;
     private static OkHttpClient okHttpClient;
+    private static Retrofit retrofit;
     private static Context mContext;
+    private static Cache cache = null;
 
     private static Retrofit.Builder builder =
             new Retrofit.Builder()
@@ -59,6 +64,23 @@ public class RetrofitClient {
         if (context != null) {
             mContext = context;
         }
+        okHttpClient = new OkHttpClient.Builder()
+                .addNetworkInterceptor(
+                        new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .cache(cache)
+                .addInterceptor(new BaseInterceptor(headers))
+                .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+                .writeTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+                .connectionPool(new ConnectionPool(8, 15, TimeUnit.SECONDS))
+                // 这里你可以根据自己的机型设置同时连接的个数和时间，我这里8个，和每个保持时间为10s
+                .build();
+        retrofit = new Retrofit.Builder()
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .baseUrl(url)
+                .build();
+
         return new RetrofitClient(context, url, headers);
     }
 
@@ -82,4 +104,12 @@ public class RetrofitClient {
             url = baseUrl;
         }
     }
+
+    public static void changeBaseUrl(String newBaseUrl) {
+        baseUrl = newBaseUrl;
+        builder = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(baseUrl);
+    }
+
 }
